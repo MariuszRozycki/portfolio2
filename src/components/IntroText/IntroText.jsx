47551be6fc9e2bf36981a8a10d8537729b4669b4
MariuszRozycki <mariansc83@icloud.com>
2025-03-10 21:32:22 +0100

    Change text in component IntroText

--- src/components/IntroText/IntroText.jsx
+++ src/components/IntroText/IntroText.jsx
@@ -2,12 +2,18 @@ import * as S from "./IntroText.styled";
 import { Typewriter } from "react-simple-typewriter";
 
 const IntroText = () => {
-  const fullText = ["Hi, my name is Mariusz Rozycki,", "Student of Frontend development at Noroff.", "Read more about me below..."];
+  const fullText = [
+    "Hi, I'm Mariusz Rozycki.",
+    "I hold a Higher Professional Degree in Frontend Development from Noroff.",
+    "I'm looking for an exciting new project where I can apply my frontend skills and creativity.",
+    "Take a look at my projects!",
+    "Learn more about me below and check out my CV.",
+  ];
 
   return (
     <S.IntroText>
       <>
-        <Typewriter words={fullText} cursor cursorStyle='|' typeSpeed={50} deleteSpeed={20} delaySpeed={1000} loop={0} />
+        <Typewriter words={fullText} cursor cursorStyle='|' typeSpeed={45} deleteSpeed={20} delaySpeed={1500} loop={0} />
       </>
     </S.IntroText>
   );
--- src/components/IntroText/IntroText.styled.jsx
+++ src/components/IntroText/IntroText.styled.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const IntroText = styled.section.attrs({ className: "intro-text-section mt-3" })`
-  height: 60px;
+  height: 65px;
   color: var(--color-denary);
 `;
